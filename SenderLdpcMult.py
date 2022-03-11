c9bf082013125ffea4e41de0e25b9520d5e89981
MGalaxy1984 <xiezffcbarcelona@gmail.com>
2022-03-10 23:30:41 -0800

    encoder

--- SenderLdpcMult.py
+++ SenderLdpcMult.py
@@ -22,16 +22,18 @@ diagMtx_g16_w5_seed1_t36 = [
     [0, 6, 10, 15]
 ]
 
-m_rows = 0
-cols = 0
+rows = 0
+r_cols = 0
+l_cols = 0
+v = []
 
 offset0 = 0
 offset1 = 0
 
 
 def r_component(x: list[Block]):
-    global offset0, offset1, main_end, m_rows
-    xi = m_rows - 1
+    global offset0, offset1, main_end, rows
+    xi = rows - 1
     xx = xi - 16
     while xi > main_end:
         for j in range(16):
@@ -40,26 +42,25 @@ def r_component(x: list[Block]):
             col2 = diagMtx_g16_w5_seed1_t36[xi & 15][2]
             col3 = diagMtx_g16_w5_seed1_t36[xi & 15][3]
 
-            # if xi >= m_rows - 17:
-            print(f'xi value = {x[xi]}')
-
-            print(f'col0 = {col0}')
-            print(f'col1 = {col1}')
-            print(f'col2 = {col2}')
-            print(f'col3 = {col3}')
-
-            print(f'xc0 = {xx + col0}')
-            print(f'xc1 = {xx + col1}')
-            print(f'xc2 = {xx + col2}')
-            print(f'xc3 = {xx + col3}')
-
-            print(f'xc0 value = {x[xx + col0]}')
-            print(f'xc1 value = {x[xx + col1]}')
-            print(f'xc2 value = {x[xx + col2]}')
-            print(f'xc3 value = {x[xx + col3]}')
-
-            print(f'offset0 = {offset0}')
-            print(f'offset1 = {offset1}')
+            # print(f'xi value = {x[xi]}')
+            #
+            # print(f'col0 = {col0}')
+            # print(f'col1 = {col1}')
+            # print(f'col2 = {col2}')
+            # print(f'col3 = {col3}')
+            #
+            # print(f'xc0 = {xx + col0}')
+            # print(f'xc1 = {xx + col1}')
+            # print(f'xc2 = {xx + col2}')
+            # print(f'xc3 = {xx + col3}')
+            #
+            # print(f'xc0 value = {x[xx + col0]}')
+            # print(f'xc1 value = {x[xx + col1]}')
+            # print(f'xc2 value = {x[xx + col2]}')
+            # print(f'xc3 value = {x[xx + col3]}')
+            #
+            # print(f'offset0 = {offset0}')
+            # print(f'offset1 = {offset1}')
 
             x[xx + col0] ^= x[xi]
             x[xx + col1] ^= x[xi]
@@ -71,44 +72,189 @@ def r_component(x: list[Block]):
             if offset1 >= 0:
                 x[offset1] ^= x[xi]
 
-            print(f'i = {xi}')
-            print(f'x[513] = {x[513]}')
-            print(f'x[514] = {x[514]}')
-            print(f'x[515] = {x[515]}')
-            print(f'x[516] = {x[516]}')
-            print(f'x[517] = {x[517]}')
-            print(f'x[518] = {x[518]}')
-            print(f'x[519] = {x[519]}')
+            # print(f'i = {xi}')
+            # print(f'x[513] = {x[513]}')
+            # print(f'x[514] = {x[514]}')
+            # print(f'x[515] = {x[515]}')
+            # print(f'x[516] = {x[516]}')
+            # print(f'x[517] = {x[517]}')
+            # print(f'x[518] = {x[518]}')
+            # print(f'x[519] = {x[519]}')
 
             offset0 -= 1
             offset1 -= 1
             xx -= 1
             xi -= 1
 
+    while xi >= 0:
+        # print(xi)
+        # print(f'xi = {xi}')
+        for j in range(4):
+            col = diagMtx_g16_w5_seed1_t36[xi & 15][j] + xi - 16
+            if col >= 0:
+                # print(f'col = {col}')
+                x[col] ^= x[xi]
+        if offset0 >= 0:
+            x[offset0] ^= x[xi]
+            offset0 -= 1
+        if offset1 >= 0:
+            x[offset1] ^= x[xi]
+            offset1 -= 1
+        xi -= 1
+
+
+def l_component(ppp: list[Block], mm: list[Block]):
+    global l_cols, rows, v
+    i = 0
+    # print(f'cols = {cols}')
+    while i < l_cols:
+        end = l_cols
+        for j in range(5):
+            if v[j] == rows:
+                v[j] = 0
+            j_end = l_cols - v[j] + i
+            end = min(end, j_end)
+        # print(f'v0 = {v[0]}')
+        # print(f'v1 = {v[1]}')
+        # print(f'v2 = {v[2]}')
+        # print(f'v3 = {v[3]}')
+        # print(f'v4 = {v[4]}')
+        tmp_i = i
+        m0 = v[0]
+        m1 = v[1]
+        m2 = v[2]
+        m3 = v[3]
+        m4 = v[4]
+        v[0] += end - i
+        v[1] += end - i
+        v[2] += end - i
+        v[3] += end - i
+        v[4] += end - i
+        i = end
+        while tmp_i != end:
+            # print(f'tmp_i = {tmp_i}')
+            # print(f'm0 = {m0}')
+            # print(f'm1 = {m1}')
+            # print(f'm2 = {m2}')
+            # print(f'm3 = {m3}')
+            # print(f'm4 = {m4}')
+            ppp[tmp_i] = ppp[tmp_i] ^ mm[m0] ^ mm[m1] ^ mm[m2] ^ mm[m3] ^ mm[m4]
+            m0 += 1
+            m1 += 1
+            m2 += 1
+            m3 += 1
+            m4 += 1
+            tmp_i += 1
+
 
 def component(m: list[Block]):
-    for i in range(cols):
-        print("hi")
+    r_component(m[r_cols - rows: r_cols])
+    l_component(m[0: r_cols - rows], m[r_cols - rows: r_cols])
+
+
+def test():
+    global rows, r_cols, l_cols, v, offset0, offset1
+    fi = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_encoder_input.txt")
+    r_cols = int(fi.readline().rstrip(), 10)
+    print(r_cols)
+    rows = int(fi.readline().rstrip(), 10)
+    print(rows)
+    m = []
+    for i in range(r_cols):
+        line = fi.readline()
+        m.append(Block(int("0x" + line.rstrip(), 16)))
+    fi.close()
+    fi = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_encoder_r_input.txt")
+    offset0 = int(fi.readline().rstrip(), 10)
+    offset1 = int(fi.readline().rstrip(), 10)
+    fi.close()
+    fi = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_encoder_l_input.txt")
+    v_size = int(fi.readline().rstrip(), 10)
+    for i in range(v_size):
+        v.append(int(fi.readline().rstrip(), 10))
+        # print(v[i])
+    l_cols = int(fi.readline().rstrip(), 10)
+    fi.close()
+    m_after = []
+    fi = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_encoder_output.txt")
+    for i in range(r_cols):
+        line = fi.readline()
+        m_after.append(Block(int("0x" + line.rstrip(), 16)))
+    fi.close()
+
+    component(m)
+
+    result = True
+    fi = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_encoder_component_output.txt", "w")
+    for i in range(r_cols):
+        if m[i] != m_after[i]:
+            result = False
+        fi.write(f'{m[i]} {m_after[i]}\n')
+    print(result)
+
+
+def l_test():
+    global rows, r_cols, v
+    fi = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_mlencoder_input.txt")
+    v_size = int(fi.readline().rstrip(), 10)
+    for i in range(v_size):
+        v.append(int(fi.readline().rstrip(), 10))
+    ppp = []
+    mm = []
+    rows = int(fi.readline().rstrip(), 10)
+    r_cols = int(fi.readline().rstrip(), 10)
+    for i in range(rows):
+        line = fi.readline()
+        ppp.append(Block(int("0x" + line.rstrip(), 16)))
+    for i in range(r_cols):
+        line = fi.readline()
+        mm.append(Block(int("0x" + line.rstrip(), 16)))
+
+    l_component(ppp, mm)
+
+    ppp_after = []
+    mm_after = []
+    fi = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_mlencoder_output.txt")
+    for i in range(rows):
+        line = fi.readline()
+        ppp_after.append(Block(int("0x" + line.rstrip(), 16)))
+    for i in range(r_cols):
+        line = fi.readline()
+        mm_after.append(Block(int("0x" + line.rstrip(), 16)))
+    fi.close()
+
+    to = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_mlencoder_component_output.txt", "w")
+    result = True
+    for i in range(rows):
+        to.write(f'{ppp[i]}\n')
+        if ppp[i] != ppp_after[i]:
+            result = False
+    for i in range(r_cols):
+        to.write(f'{mm[i]}\n')
+        if mm[i] != mm_after[i]:
+            result = False
+    to.close()
+    print(result)
 
 
 def r_test():
-    global m_rows, offset0, offset1
+    global rows, offset0, offset1
     fi = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_mrencoder_input.txt")
-    m_rows = int(fi.readline().rstrip(), 10)
+    rows = int(fi.readline().rstrip(), 10)
     # print(m_rows)
     offset0 = int(fi.readline().rstrip(), 10)
     # print(offset0)
     offset1 = int(fi.readline().rstrip(), 10)
     # print(offset1)
     m = []
-    for i in range(m_rows):
+    for i in range(rows):
         line = fi.readline()
         m.append(Block(int("0x" + line.rstrip(), 16)))
         # print(m[i])
     fi.close()
     m_after = []
     fi = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_mrencoder_output.txt")
-    for i in range(m_rows):
+    for i in range(rows):
         line = fi.readline()
         m_after.append(Block(int("0x" + line.rstrip(), 16)))
     fi.close()
@@ -117,7 +263,7 @@ def r_test():
     r_component(m)
     to = open("E:\PycharmProjects\libOTe_psuedo\DataFiles\sender_mrencoder_component_output.txt", "w")
     result = True
-    for i in range(m_rows):
+    for i in range(rows):
         to.write(f'{m[i]} {m_after[i]}\n')
         if m[i] != m_after[i]:
             result = False
--- main.py
+++ main.py
@@ -5,8 +5,18 @@ from Block import Block
 import SenderHash
 from PseudoAES import PseudoAES
 
+
+def ti(m: list[int]):
+    m[0] = 15
+    m[1] = 20
+
+
 if __name__ == '__main__':
     # SenderFerretMalCheck.test()
     # SenderHash.test()
-    SenderLdpcMult.r_test()
-
+    # SenderLdpcMult.test()
+    # SenderLdpcMult.l_test()
+    # SenderLdpcMult.r_test()
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    ti(a[3:7])
+    print(a)
